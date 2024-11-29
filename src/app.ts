479c1587ab25b7c62cb2b4ad4ba7a7ecccc78522
Bela Home <ti@belahome.com.br>
2024-11-29 16:22:22 -0300

    create player routes

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,12 @@ app.get("/cards", async (req, res) => {
   return res.send(cards);
 });
 
+app.get("/players", async (req, res) => {
+  const players = await prisma.player.findMany();
+
+  return res.send(players);
+});
+
 app.post("/cards", async (req, res) => {
   const bodySchema = z.object({
     type: z.enum(["TRUTH", "CHALLENGE"]),
@@ -28,6 +34,22 @@ app.post("/cards", async (req, res) => {
   return res.send({ message: "Card criado com sucesso!" });
 });
 
+app.post("/players", async (req, res) => {
+  const bodySchema = z.object({
+    name: z.string(),
+  });
+
+  const { name } = bodySchema.parse(req.body);
+
+  await prisma.player.create({
+    data: {
+      name,
+    },
+  });
+
+  return res.send({ message: "Jogador criado com sucesso!" });
+});
+
 app.delete("/cards/:id", async (req, res) => {
   const paramsSchema = z.object({
     id: z.string().uuid(),
@@ -36,6 +58,22 @@ app.delete("/cards/:id", async (req, res) => {
   const { id } = paramsSchema.parse(req.params);
 
   await prisma.card.delete({
+    where: {
+      id,
+    },
+  });
+
+  return res.status(204).send();
+});
+
+app.delete("/players/:id", async (req, res) => {
+  const paramsSchema = z.object({
+    id: z.string().uuid(),
+  });
+
+  const { id } = paramsSchema.parse(req.params);
+
+  await prisma.player.delete({
     where: {
       id,
     },
